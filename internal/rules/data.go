ec5fda83f1e89025023f2abc91fea783fa739b44
ShubhamK-8 <shubham.k8@delhivery.com>
2025-06-01 22:41:49 +0530

    fix model error

--- internal/rules/data.go
+++ internal/rules/data.go
@@ -3,13 +3,13 @@ package rules
 import "targeting-engine/internal/models"
 
 var Campaigns = []models.Campaign{
-    {"spotify", "https://somelink", "Download", "ACTIVE"},
-    {"duolingo", "https://somelink2", "Install", "ACTIVE"},
-    {"subwaysurfer", "https://somelink3", "Play", "ACTIVE"},
+	{"spotify", "https://somelink", "Download", "ACTIVE"},
+	{"duolingo", "https://somelink2", "Install", "ACTIVE"},
+	{"subwaysurfer", "https://somelink3", "Play", "ACTIVE"},
 }
 
 var Rules = []models.TargetingRule{
-    {CampaignID: "spotify", IncludeCountry: []string{"US", "Canada"}},
-    {CampaignID: "duolingo", IncludeOS: []string{"Android", "iOS"}, ExcludeCountry: []string{"US"}},
-    {CampaignID: "subwaysurfer", IncludeOS: []string{"Android"}, IncludeAppIDs: []string{"com.gametion.ludokinggame"}},
+	{CampaignID: "spotify", IncludeCountry: []string{"US", "Canada"}},
+	{CampaignID: "duolingo", IncludeOS: []string{"Android", "iOS"}, ExcludeCountry: []string{"US"}},
+	{CampaignID: "subwaysurfer", IncludeOS: []string{"Android"}, IncludeAppIDs: []string{"com.gametion.ludokinggame"}},
 }
